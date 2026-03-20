13ad65dd698788a375f763898c09959508cfd5f6
Kacper <kacperstrzalkowski13@gmail.com>
2026-03-20 21:48:26 +0100

    Add error handling for missing HTML directory in routing function

--- src/models/router.rs
+++ src/models/router.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::path::PathBuf;
+use std::process::exit;
 
 pub fn get_routing() -> Result<HashMap<String, PathBuf>, std::io::Error> {
     let mut router_map: HashMap<String, PathBuf> = HashMap::new();
@@ -9,6 +10,11 @@ pub fn get_routing() -> Result<HashMap<String, PathBuf>, std::io::Error> {
     let exe_path = env::current_exe()?;
     let folder_path = exe_path.parent().expect("Invalid parent folder name");
     let folder_to_seek_path = folder_path.join("html");
+
+    if !folder_to_seek_path.exists() {
+        eprintln!("Create a html directory first!");
+        exit(1);
+    }
 
     for entry in fs::read_dir(folder_to_seek_path)? {
         let entry = entry?;
